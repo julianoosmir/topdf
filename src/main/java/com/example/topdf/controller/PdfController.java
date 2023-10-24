3b52a491de37cb36d0b5f87b3db0396976207ba1
juliano <julianoosmir@outlook.com>
2023-10-24 19:51:25 -0400

    templete deu certo

--- src/main/java/com/example/topdf/controller/PdfController.java
+++ src/main/java/com/example/topdf/controller/PdfController.java
@@ -1,5 +1,7 @@
 package com.example.topdf.controller;
 
+import com.example.topdf.model.DataMapper;
+import com.example.topdf.model.Employee;
 import com.example.topdf.services.PdfService;
 
 import jakarta.servlet.http.HttpServletResponse;
@@ -8,9 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class PdfController {
@@ -18,6 +24,11 @@ public class PdfController {
     @Autowired
     private PdfService pdfService;
 
+    @Autowired
+    private DataMapper dataMapper;
+
+    @Autowired
+    private SpringTemplateEngine springTemplateEngine;
     @GetMapping("/")
     public String index() {
         return "index";
@@ -30,5 +41,20 @@ public class PdfController {
         response.setHeader("Content-Disposition", "attachment; filename=file.pdf");
         IOUtils.copy(byteArrayInputStream, response.getOutputStream());
     }
+    @PostMapping(value = "/generateDocument")
+    public void generateDocument(HttpServletResponse response) throws IOException {
+        List<Employee> employeeList = Arrays.asList(new Employee("teste","testando","teste@test"));
 
+        String finalHtml = null;
+
+        Context dataContext = dataMapper.setData(employeeList);
+
+        finalHtml = springTemplateEngine.process("template", dataContext);
+
+        ByteArrayInputStream byteArrayInputStream = pdfService.convertHtmlToPdf(finalHtml);
+
+        response.setContentType("application/octet-stream");
+        response.setHeader("Content-Disposition", "attachment; filename=file.pdf");
+        IOUtils.copy(byteArrayInputStream, response.getOutputStream());
+    }
 }
---
+++ src/main/java/com/example/topdf/model/DataMapper.java
@@ -0,0 +1,25 @@
+package com.example.topdf.model;
+
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class DataMapper {
+
+    public Context setData(List<Employee> empolyeeList) {
+
+        Context context = new Context();
+
+        Map<String, Object> data = new HashMap<>();
+
+        data.put("employees", empolyeeList);
+
+        context.setVariables(data);
+
+        return context;
+    }
+}
